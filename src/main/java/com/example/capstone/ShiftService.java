9191b57ad6b649bce5d08e966a7ba51693cfb53b
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2021-07-16 11:51:57 -0500

    fixed uikit showing & working on adding shift component

---
+++ src/main/java/com/example/capstone/ShiftService.java
@@ -0,0 +1,28 @@
+package com.example.capstone;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+@Transactional
+public class ShiftService {
+
+    @Autowired
+    private ShiftRepository repo;
+
+    public static Date between(Date startInclusive, Date endExclusive) {
+        long startMillis = startInclusive.getTime();
+        long endMillis = endExclusive.getTime();
+        long randomMillisSinceEpoch = ThreadLocalRandom
+                .current()
+                .nextLong(startMillis, endMillis);
+
+        return new Date(randomMillisSinceEpoch);
+    }
+
+}
